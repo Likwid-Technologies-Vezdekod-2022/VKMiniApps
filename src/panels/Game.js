6f083827dc59c96d0379daf25047b6c4a5756a32
wariiandr <wariiandr>
2022-06-04 18:04:15 +0300

    перелистывание карточек

--- src/panels/Game.js
+++ src/panels/Game.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Panel, PanelHeader, PanelHeaderBack, Div, Group, CardGrid, Card, Header, ContentCard } from '@vkontakte/vkui';
+import { Panel, PanelHeader, PanelHeaderBack, Div, Group, CardGrid, Card, Header, ContentCard, Button } from '@vkontakte/vkui';
 
 import './Game.css';
 
@@ -8,6 +8,16 @@ const Game = ({id, go, deck}) => {
 	const [currentCardIdx, setCurrentCardIdx] = useState(0);
 	const [isBlock, setBlock] = useState(false);
 
+	const currentCard = deck[currentCardIdx];
+
+	const block = () => {
+		setBlock(true);
+	}	
+
+	const changeIdx = () => {
+		setCurrentCardIdx(currentCardIdx + 1);
+		setBlock(false);
+	}
 
 	return (
 		<Panel id={id}>
@@ -18,19 +28,38 @@ const Game = ({id, go, deck}) => {
 			</PanelHeader>
 
 			<Div>
-				{deck.map((card, idx) =>
+				{!isBlock && <Div>
 					<Group mode="plain"
 						header={<Header mode="secondary">Ваша карта: </Header>}>
 							
 						<CardGrid size="l">
 							<ContentCard
-								subtitle={card.title && `Локация: ${card.title}`}
-								header={`Статус: ${card.status}`}
+								subtitle={currentCard.title && `Локация: ${currentCard.title}`}
+								header={`Статус: ${currentCard.status}`}
 							/>
+						</CardGrid>
 
+						<Button stretched size="l" mode="secondary" onClick={block}>
+							Дальше
+						</Button>
+					</Group>
+				</Div>}
+
+				{isBlock && <Div>
+					<Group mode="plain"
+						header={<Header mode="secondary">Не подглядывайте!</Header>}>
+							
+						<CardGrid size="l">
+							<ContentCard
+								header="Передайте телефон другому игроку!"
+							/>
 						</CardGrid>
 					</Group>
-				)}
+
+					<Button stretched size="l" mode="secondary" onClick={changeIdx}>
+						Все ок
+					</Button>
+				</Div>}
 			</Div>
 
 		</Panel>
